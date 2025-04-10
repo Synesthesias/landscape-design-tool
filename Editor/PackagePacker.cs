d80f689d3987474c78ff0eb8953a325bcc059687
yhikishima <lallapalooza1120@gmail.com>
2025-04-10 16:09:24 +0900

    add:tgzファイル作成のメニュー追加

---
+++ Editor/PackagePacker.cs
@@ -0,0 +1,21 @@
+﻿using PLATEAU.Util;
+using UnityEditor;
+using UnityEditor.PackageManager;
+
+namespace PLATEAU.Editor.DebugPlateau
+{
+    /// <summary>
+    /// Packagesフォルダに入っているLandscape Design Tool を tarball 形式で出力します。
+    /// デプロイで利用します。
+    /// </summary>
+    static class PackagePacker
+    {
+        private const string PACKAGE_NAME = "com.synesthesias.landscape-design-tool";
+        [MenuItem("PLATEAU/Debug/Pack Landscape Package to tarball")]
+        public static void PackLandScape()
+        {
+            string destDir = EditorUtility.SaveFolderPanel("出力先", "", "");
+            Client.Pack($"Packages/{PACKAGE_NAME}", destDir);
+        }
+    }
+}
